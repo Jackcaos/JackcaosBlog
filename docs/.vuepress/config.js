2d29831df589079ba45d47432ff452889b8cb7aa
caosheng14 <caosheng14@jd.com>
2021-05-20 11:38:48 +0800

    feat: 修改项目名

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,7 +19,7 @@ module.exports = {
               collapsable: true,
               children: ['functional']
             },
-            'home2'
+            'lighthouse'
           ],
           '/react/': [
             {
